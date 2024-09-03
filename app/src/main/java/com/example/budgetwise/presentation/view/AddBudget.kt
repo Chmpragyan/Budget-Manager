2d82c52a62b398aa1b423d66b97f4f3e5adc9d21
Chmpragyan <pragyancham07@github.com>
2024-09-03 10:39:25 +0545

    feat: layout for income, expense and transfer budget implemented

--- app/src/main/java/com/example/budgetwise/presentation/view/AddBudget.kt
+++ app/src/main/java/com/example/budgetwise/presentation/view/AddBudget.kt
@@ -1,12 +1,14 @@
 package com.example.budgetwise.presentation.view
 
-import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.FrameLayout
+import androidx.annotation.StringRes
+import androidx.appcompat.widget.AppCompatButton
 import com.example.budgetwise.R
 import com.example.budgetwise.databinding.FragmentAddBudgetBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -34,7 +36,38 @@ class AddBudget : BottomSheetDialogFragment(), View.OnClickListener {
 
         displayBottomSheet()
 
+        handleButtonClick(
+            selectedButton = binding.addIncomeBtn,
+            deselectedButtons = listOf(binding.addExpenseBtn, binding.addTransferBtn),
+            titleResId = R.string.add_income,
+            visibleLayout = binding.flIncomeLayout,
+            hiddenLayouts = listOf(binding.flExpenseLayout, binding.flTransferLayout)
+        )
+
         initializeButtonClick()
+
+        implementDropdown()
+    }
+
+    private fun setupCategory(autoCompleteTextView: AutoCompleteTextView, arrayResId: Int) {
+        val category = resources.getStringArray(arrayResId)
+        val arrayAdapter = ArrayAdapter(
+            requireContext(),
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+            category
+        )
+        autoCompleteTextView.setAdapter(arrayAdapter)
+    }
+
+    private fun implementDropdown() {
+        setupCategory(binding.expenseLayout.autoCompleteTextView, R.array.expense_category)
+        setupCategory(binding.expenseLayout.accountCategory, R.array.account_category)
+
+        setupCategory(binding.incomeLayout.autoCompleteTextView, R.array.income_category)
+        setupCategory(binding.incomeLayout.accountCategory, R.array.account_category)
+
+        setupCategory(binding.transferLayout.autoCompleteTextView, R.array.account_category)
+        setupCategory(binding.transferLayout.accountCategory, R.array.account_category)
     }
 
     private fun initializeButtonClick() {
@@ -65,34 +98,55 @@ class AddBudget : BottomSheetDialogFragment(), View.OnClickListener {
 
     override fun onClick(view: View?) {
         when (view?.id) {
-            R.id.add_income_btn -> {
-                binding.apply {
-                    toolbar.title = requireContext().getString(R.string.add_income)
-                    addIncomeBtn.isSelected = true
-                    addExpenseBtn.isSelected = false
-                    addTransferBtn.isSelected = false
-                }
-            }
+            R.id.add_income_btn -> handleButtonClick(
+                selectedButton = binding.addIncomeBtn,
+                deselectedButtons = listOf(binding.addExpenseBtn, binding.addTransferBtn),
+                titleResId = R.string.add_income,
+                visibleLayout = binding.flIncomeLayout,
+                hiddenLayouts = listOf(binding.flExpenseLayout, binding.flTransferLayout)
+            )
 
-            R.id.add_expense_btn -> {
-                binding.apply {
-                    toolbar.title = requireContext().getString(R.string.add_expense)
-                    addIncomeBtn.isSelected = false
-                    addExpenseBtn.isSelected = true
-                    addTransferBtn.isSelected = false
-                }
-            }
+            R.id.add_expense_btn -> handleButtonClick(
+                selectedButton = binding.addExpenseBtn,
+                deselectedButtons = listOf(binding.addIncomeBtn, binding.addTransferBtn),
+                titleResId = R.string.add_expense,
+                visibleLayout = binding.flExpenseLayout,
+                hiddenLayouts = listOf(binding.flIncomeLayout, binding.flTransferLayout)
+            )
 
-            R.id.add_transfer_btn -> {
-                binding.apply {
-                    toolbar.title = requireContext().getString(R.string.add_transfer)
-                    addIncomeBtn.isSelected = false
-                    addExpenseBtn.isSelected = false
-                    addTransferBtn.isSelected = true
-                }
-            }
+            R.id.add_transfer_btn -> handleButtonClick(
+                selectedButton = binding.addTransferBtn,
+                deselectedButtons = listOf(binding.addIncomeBtn, binding.addExpenseBtn),
+                titleResId = R.string.add_transfer,
+                visibleLayout = binding.flTransferLayout,
+                hiddenLayouts = listOf(binding.flIncomeLayout, binding.flExpenseLayout)
+            )
         }
     }
 
+    private fun handleButtonClick(
+        selectedButton: AppCompatButton,
+        deselectedButtons: List<AppCompatButton>,
+        @StringRes titleResId: Int,
+        visibleLayout: View,
+        hiddenLayouts: List<View>
+    ) {
+        binding.apply {
+            toolbar.title = requireContext().getString(titleResId)
+
+            selectedButton.isSelected = true
+            selectedButton.setTextColor(requireContext().getColor(R.color.button_pressed))
+
+            deselectedButtons.forEach { button ->
+                button.isSelected = false
+                button.setTextColor(requireContext().getColor(R.color.white))
+            }
+
+            visibleLayout.visibility = View.VISIBLE
+            hiddenLayouts.forEach { layout ->
+                layout.visibility = View.GONE
+            }
+        }
+    }
 
 }
